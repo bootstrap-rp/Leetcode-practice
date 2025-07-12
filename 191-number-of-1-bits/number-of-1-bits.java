4d60c48521c073dc2905df76584a5f3a559fd321
bootstrap-rp <rudrapatel15067@gmail.com>
2025-07-12 07:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (30.98%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        for(int i=0;i<32;i++){
+            int bitmask=1<<i;
+            if((bitmask & n)!=0){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
