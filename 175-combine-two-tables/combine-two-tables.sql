3954735216d8f95cc2c0385d395984b229065859
bootstrap-rp <rudrapatel15067@gmail.com>
2026-03-16 21:42:06 +0530

    Time: 408 ms (41.49%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select p.firstName,p.lastName,a.city,a.state
-from Person p left join Address a
+from Person as p
+left join Address as a
 on p.personId=a.personId;
